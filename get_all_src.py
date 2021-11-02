835031aa8c1f479e3f0299b67b8ed503617e12d1
sycct <huang_9119596@163.com>
2021-11-02 14:30:24 +0800

    2021-11-02 爬虫系列：保存媒体文件源代码。

--- get_all_src.py
+++ get_all_src.py
@@ -2,34 +2,35 @@
 # -*- coding: UTF-8 -*-
 
 import os.path
-from urllib.request import urlretrieve
+import requests
+
 from utils import connection_util
 
 
 class GetAllSrc(object):
     def __init__(self):
         self._init_download_dir = 'downloaded'
-        self._baseUrl = 'https://www.pdflibr.com/ip'
+        self.__targetUrl = 'https://www.scrapingbee.com/blog/'
+        self._baseUrl = 'https://www.scrapingbee.com'
         self._init_connection = connection_util.ProcessConnection()
 
     @staticmethod
     def get_absolute_url(baseUrl, source):
-        if source.startswith("https://image."):
-            url = "https://" + source[14:]
+        if source.startswith("https://www."):
+            url = "https://www.scrapingbee.com" + source[27:]
         elif source.startswith("https://"):
             url = source
         elif source.startswith("www."):
             url = "https://" + source[4:]
         else:
-            url = source
-        if baseUrl not in url:
+            url = baseUrl + '/' + source
+        if baseUrl not in source[0:27]:
             return None
         return url
 
     @staticmethod
     def get_download_path(baseUrl, absoluteUrl, download_dir):
-        path = absoluteUrl.replace("www.", "")
-        path = path.replace(baseUrl, "")
+        path = absoluteUrl.replace(baseUrl, "")
         path = download_dir + path
         directory = os.path.dirname(path)
 
@@ -39,14 +40,16 @@ class GetAllSrc(object):
         return path
 
     def download_main(self):
-        get_content = self._init_connection.init_connection(self._baseUrl)
+        get_content = self._init_connection.init_connection(self.__targetUrl)
         if get_content:
             download_list = get_content.findAll(src=True)
             for download in download_list:
                 file_url = self.get_absolute_url(self._baseUrl, download["src"])
                 if file_url is not None:
                     print(file_url)
-                    urlretrieve(file_url, self.get_download_path(self._baseUrl, file_url, self._init_download_dir))
+                    r = requests.get(file_url)
+                    with open(self.get_download_path(self._baseUrl, file_url, self._init_download_dir), 'wb') as f:
+                        f.write(r.content)
 
 
 if __name__ == '__main__':
